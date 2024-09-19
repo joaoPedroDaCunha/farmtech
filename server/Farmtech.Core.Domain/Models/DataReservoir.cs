174a062671b82f7c7d45b7ff7e18ca3cbe8d6417
unknown <joaopedrodacunha.santos@gmail.com>
2024-09-19 15:43:27 -0300

    adicionando o id no DataReservoir

--- server/Farmtech.Core.Domain/Models/DataReservoir.cs
+++ server/Farmtech.Core.Domain/Models/DataReservoir.cs
@@ -2,10 +2,13 @@ namespace Farmtech.Core.Domain.Models;
 
 public class DataReservoir
 {
+    //id unico da informação
+    public int id {get;set;}
      // inicio das variaves utilizadas para as regras de negocio
     public DateTime data {get;set;}
     public decimal capacidade{get;set;}
     public decimal altura{get;set;}
     public decimal volume_atual{get;set;}
     //fim das variaves utilizadas para as regras de negocio
+    public int foreign_key
 }
