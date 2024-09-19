c74cde41b3d06d0e56a74eda3e6a740362dc77ed
Gustavo Henrico <gustavohenrico58@gmail.com>
2024-09-18 21:00:25 -0300

    Primeiro commit do server

---
+++ server/Farmtech.Core.Domain/Models/Workspace.cs
@@ -0,0 +1,9 @@
+
+namespace Farmtech.Core.Domain.Models;
+
+public class Workspace
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+};
