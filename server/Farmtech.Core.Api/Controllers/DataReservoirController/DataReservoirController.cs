eee7bb5dc654b1d4c750f559c1b7de3fd9061607
unknown <joaopedrodacunha.santos@gmail.com>
2024-09-23 09:29:21 -0300

    Controller DataReservoir

---
+++ server/Farmtech.Core.Api/Controllers/DataReservoirController/DataReservoirController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class DataReservoirController : Controller
+    {
+        // GET: DataReservoirController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
