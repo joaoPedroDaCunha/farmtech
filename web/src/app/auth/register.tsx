e8681bd2f806a44a8ecaa40624d00193c7128c39
Rafael Moreira <moreirarafinha30@gmail.com>
2024-09-26 22:57:24 -0300

    Adiciona tela de cadastro

---
+++ web/src/app/auth/register.tsx
@@ -0,0 +1,40 @@
+
+import { Button, Input, } from "@material-tailwind/react"
+import { FcGoogle } from "react-icons/fc";
+
+
+export const Register = () => {
+
+
+    return (
+
+        <div className="bg-white p-10 gap-4 flex flex-col rounded-lg shadow-lg max-w-md w-full">
+            <h1 className="text-3xl font-semibold my-4">Create Account</h1>
+            <Input size="lg" color="blue" label="Name" />
+            <Input size="lg" color="blue" label="Email" />
+            <Input size="lg" color="blue" label="Password" />
+            <Button color="blue" className="my-2">Submit</Button>
+
+            <div className="relative flex  py-2 items-center">
+                <div className="flex-grow border-t border-gray-400"></div>
+                <span className="flex-shrink mx-4 text-sm text-gray-400">Or</span>
+                <div className="flex-grow border-t border-gray-400"></div>
+            </div>
+
+            <Button
+                variant="outlined"
+                size="lg"
+                className="flex h-12 border-blue-gray-200 items-center justify-center gap-2 "
+                fullWidth>
+                <FcGoogle size={20} />
+                Sign in with Google
+                
+            </Button>
+
+        </div>
+
+
+
+
+    )
+}
--- web/src/app/root/home.tsx
+++ web/src/app/root/home.tsx
@@ -1,16 +1,10 @@
-import { Button, Checkbox, Input, Progress } from "@material-tailwind/react"
-
 
 
 export const Home = () => {
 
 
     return (
-        <div>
-            <Input label="Email" color="green" />
-            <Progress color="green" value={3} />
-            <Checkbox color="green" />
-            <Button color="teal">Button</Button>
-        </div>
+        <div></div>
+
     )
 }
