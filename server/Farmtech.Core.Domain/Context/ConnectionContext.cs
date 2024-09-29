aa8bbcb3784e7e9b023594e5c63e3be890dfa9f7
Gustavo Henrico <gustavohenrico58@gmail.com>
2024-09-29 01:28:42 -0300

    Resolve problema com conectividade ao sqlite

--- server/Farmtech.Core.Domain/Context/ConnectionContext.cs
+++ server/Farmtech.Core.Domain/Context/ConnectionContext.cs
@@ -4,13 +4,13 @@ namespace Farmtech.Core.Domain.Models;
 
 public class ConnectionContext : DbContext
 {
-    public DbSet<Workspace> Workspaces {get;set;}
+    public DbSet<Workspace> Workspaces { get; set; }
 
-    public DbSet<User> Users {get;set;}
+    public DbSet<User> Users { get; set; }
 
-    public DbSet<Member> Members {get;set;}
+    public DbSet<Member> Members { get; set; }
 
-    public DbSet<Reservoir> Reservoirs {get;set;}
+    public DbSet<Reservoir> Reservoirs { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
      => options.UseSqlite($"Data Source=./Data.db");
